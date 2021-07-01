1a3dcf58a00d76e91b4ed7bd94c15197d9f4eeb6
Gilsmon <gilscherian13@gmail.com>
2021-07-01 10:18:03 +0530

    enabled local storage for cards

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 // creating a function to get data from modal
 
 const taskContainer = document.querySelector(".task__container");    //selecting row by className
-console.log(taskContainer);
+
+//  create an array of object to store card details
+
+const golbalStore = [];
 
 //  insert taskdata to the html code using templete literal
 const createNewCard = (taskData) => `
@@ -39,6 +42,25 @@ const createNewCard = (taskData) => `
 </div>
 `;
 
+const loadInitialCardData = () => {
+
+  // getting card details from local storage
+  const getCardData = localStorage.getItem("appID");
+
+  // convert from string to normal object 
+  const {cards} = JSON.parse(getCardData);   // destructring
+
+  // loop over those array of task object to create html card and inject it
+
+  cards.map((cardObject) =>{
+
+    taskContainer.insertAdjacentHTML("beforeend", createNewCard(cardObject) );
+
+    //  update our global storage with previous datas from localstorage
+    golbalStore.push(cardObject);
+  });
+}
+
 
 
 const saveChange = () => {
@@ -54,6 +76,14 @@ const saveChange = () => {
 
     // insert the HTML elements to the DOM using javascript
   taskContainer.insertAdjacentHTML("beforeend", createNewCard(taskData));  
+
+  golbalStore.push(taskData);
+
+  //  storing in local storage
+  // local storage has best support for stringified objects
+  // stringify expect an object (cards is a new object and globalStore is an array)
+
+  localStorage.setItem("appID",JSON.stringify({cards:golbalStore}));
 
 };
 
